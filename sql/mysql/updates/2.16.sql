eb69ddbb014514ac241e0d12c22ee6d7b75a7a76
Pascal Starkl <pascal@starkl.de>
2019-11-03 15:48:10 +0100

    feat: added sql files and dataTables css

---
+++ sql/mysql/updates/2.16.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `#__maplatlong`;
+
+CREATE TABLE `#__maplatlong` (
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+	`item_id` INT(11) NOT NULL,
+	`lat` text NOT NULL,
+	`long` text NOT NULL,
+	PRIMARY KEY (`id`)
+)
+    ENGINE=InnoDB
+    AUTO_INCREMENT =0
+    DEFAULT CHARSET=utf8mb4
+    DEFAULT COLLATE=utf8mb4_unicode_ci;
